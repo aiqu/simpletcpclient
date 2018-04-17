82f13f31d9d4ca5463df95b19ecea459221c87e0
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-04-17 18:36:02 +0900

    Remove unnecessary code

--- app/src/main/java/com/naverlabs/gwangmin/sampleping/Client.java
+++ app/src/main/java/com/naverlabs/gwangmin/sampleping/Client.java
@@ -53,37 +53,6 @@ public class Client {
         }
     }
 
-    public int sendMessage() {
-        System.out.println("Inside SendMessage");
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String msg = null;
-        ByteBuffer bytebuf = ByteBuffer.allocate(1024);
-        int nBytes = 0;
-        try {
-            msg = in.readLine();
-            System.out.println("msg is " + msg);
-            bytebuf = ByteBuffer.wrap(msg.getBytes());
-            nBytes = client.write(bytebuf);
-            System.out.println("nBytes is " + nBytes);
-            if (msg.equals("quit") || msg.equals("shutdown")) {
-                System.out.println("time to stop the client");
-                interruptThread();
-                try {
-                    Thread.sleep(5000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                client.close();
-                return -1;
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Wrote " + nBytes + " bytes to the server");
-        return nBytes;
-    }
-
     public void receiveMessage() {
         if (client != null) {
             rt = new RecvThread("Receive Thread", client);
