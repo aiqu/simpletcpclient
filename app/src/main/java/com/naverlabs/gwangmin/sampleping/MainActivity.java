a241ffc48fd3339c2680ee388f9bb488a44e1910
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-06-27 15:09:41 +0900

    Add id change menu

--- app/src/main/java/com/naverlabs/gwangmin/sampleping/MainActivity.java
+++ app/src/main/java/com/naverlabs/gwangmin/sampleping/MainActivity.java
@@ -1,10 +1,16 @@
 package com.naverlabs.gwangmin.sampleping;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.EditText;
+
 import java.util.UUID;
 
 
@@ -32,5 +38,38 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.change_id:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                final EditText editText = new EditText(this);
+                builder.setTitle("Change ID")
+                        .setView(editText)
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            final String newID = editText.getText().toString();
+                            if (!newID.isEmpty()) {
+                                GUID = newID;
+                                getPreferences(MODE_PRIVATE).edit().putString("ID", GUID).commit();
+                                Log.d("GMLEE", "ID: " + GUID);
+                                getSupportActionBar().setTitle(GUID);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
